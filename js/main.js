77ca6307d51c17b7b47345aae726733f3c5f66cb
razumeyka <33911265+razumeyka@users.noreply.github.com>
2018-09-02 10:27:55 +0300

    without 7 items

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	})
 	
 	$('.select li').click(function(){
-		$(this).closest('.select').find('input').val($(this).data('value'));
+        $(this).closest('.select').find('input').val($(this).data('value'));
 		$(this).closest('.select').find('.select_field').html($(this).html());
 		$(this).closest('.select').toggleClass('active');
 	})	 
@@ -15,9 +15,11 @@ $(document).ready(function(){
 //mobile_menu
 
     $( '.mobile_menu' ).click( function() { 
-        $(this).toggleClass('active'); 
         $('header .menu').slideToggle(300);
-
+    });
+    
+    $( '.mobile_search' ).click( function() { 
+        $('.wpcf7').toggleClass('active');
     });
 
 // submenu
@@ -35,9 +37,32 @@ $(document).ready(function(){
 
     $('#coin-slider').coinslider({
         width: 1230,
-        height: 470,
+        height: 500,
         opacity: 1,
     });
+    
+// slider for insta photos
+    
+    $('.carousel .next,.carousel .prev').click(function(){
+        console.log('click carousel');
+        var countslides=jQuery('.review_container').children().children().length-Math.round(parseFloat(jQuery('.review_wrapper').css('width'))/parseFloat(jQuery('.one_doc').css('width')));
+        var slide=$(this).closest('.carousel').data('slide');
+        console.log(countslides);
+        if($(this).hasClass('next')){
+            slide++;
+            if(slide>countslides)slide=0;
+        }
+        if($(this).hasClass('prev')){
+            slide--;
+            if(slide<0)slide=countslides;
+        }
+         $(this).closest('.carousel').find('.long').css('margin-left','-'+(slide*25)+'%');
+		$(this).closest('.carousel').data('slide',slide);
+    });
+
+// fancybox
+    
+    $('.fancybox').fancybox();
 });
 
 
