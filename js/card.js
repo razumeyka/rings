29ecc487a7a21e6cdad0a18bb38b5d461f81dea4
shiziksama <karepinandrei@gmail.com>
2018-11-06 16:34:27 +0200

    added some product slider functional

--- js/card.js
+++ js/card.js
@@ -1,41 +1,81 @@
 $(document).ready(function(){
-	var initialX=0;
-	var currentX=0;
-	var offsetX=0;
-	$('.slides_list a').click(function(e){
+	if(window.innerWidth<1024){
+		var initialX=0;
+		var currentX=0;
+		//var offsetX=0;
+		var activeslide=0;
+		var active = false;
 		
-		e.preventDefault();
-		if($('.big_image .img').is(':visible')){
-			$('.big_image .img').css('background','url('+$(this).attr('href')+') no-repeat center').css('background-size','cover');
+		$('.slides_list').css('width',$('.slides_list .slide').length+'00%');
+		$('.slides_list .slide').css('width',1/$('.slides_list .slide').length*100+'%');
+		
+		$('.super_slider').on("touchstart mousedown", function(e){
+			e.preventDefault();
+			active=true;
+			var width=$('.slides_list .slide').width()
+			$('.slides_list').addClass('notrans');
+			if (e.type === "touchmove") {
+			 initialX = e.touches[0].clientX - width*activeslide;
+			}else{
+			 initialX = e.clientX - width*activeslide;
+			}
+		});
+		
+		$('.super_slider').on("touchmove mousemove", function(e){
+			
+			e.preventDefault();
+			if(!active)return;
+			if (e.type === "touchmove") {
+			  currentX = e.touches[0].clientX - initialX;
+			} else {
+			  currentX = e.clientX - initialX;
+			}
+			$('.slides_list').css('transform','translateX('+currentX+'px)')
+			
+		});
+		$('body').on('touchend mouseup',function(e){
+			e.preventDefault();
+			active=false
+			$('.slides_list').removeClass('notrans');
+			var width=$('.slides_list .slide').width()
+			var count=$('.slides_list .slide').length-1;
+			var slide=Math.round(currentX/width);
+			if(slide>0)slide=0;
+			if(slide<-count-1) slide=-count;
+			currentX=slide*width;
+			$('.super_slider .control').removeClass('active');
+			$('.super_slider .control').eq(slide-1).addClass('active');
+			$('.slides_list').css('transform','translateX('+slide/(count+1)*100+'%)');
+			activeslide=slide;
+		});
+		$('.slides_list a').click(function(e){
+			e.preventDefault();
+		});
+	}else{
+		var activeslide=0;
+		function setdesktopslide(index){
+			$('.slides_list').css('transform','translateY(-'+index*$('.slides_list .slide').height()+'px)');
 		}
-	});
-	
-	$('.super_slider').on("touchstart", function(e){
-		$('.slides_list').addClass('notrans');
-		 initialX = e.touches[0].clientX - offsetX;
-		 console.log('dragstart');
-	});
-	$('.super_slider').on("touchmove", function(e){
-		e.preventDefault();
-		if (e.type === "touchmove") {
-          currentX = e.touches[0].clientX - initialX;
-        } else {
-          currentX = e.clientX - initialX;
-        }
-		
-		$('.slides_list').css('transform','translateX('+currentX+'px)')
-		
-	});
-	$('.super_slider').on('touchend',function(e){
-		e.preventDefault();
-		$('.slides_list').removeClass('notrans');
-		var width=500;//Надо сделать по-человечески
-		var count=3-1;
-		var slide=Math.round(currentX/width);
-		if(slide>0)slide=0;
-		if(slide<-count) slide=-count;
-		currentX=slide*width;
-		$('.slides_list').css('transform','translateX('+currentX+'px)')
-		offsetX=currentX;
-	})
+		$('.card__controls .card__control').click(function(){
+			count-=$('.slides_list').height()/$('.slides_list .slide').height();
+			count=Math.ceil(count);
+			if($(this).hasClass('card__control_next'))activeslide++;
+			if($(this).hasClass('card__control_prev'))activeslide--;
+			if(activeslide<0)activeslide=0;
+			if(activeslide>count)activeslide=count;
+			setdesktopslide(activeslide);
+			
+			
+		});
+		$('.slides_list a').click(function(e){
+			
+			e.preventDefault();
+			$('.slides_list a').removeClass('active');
+			$(this).addClass('active');
+			if($('.big_image .img').is(':visible')){
+				$('.big_image .img').css('background','url('+$(this).attr('href')+') no-repeat center').css('background-size','cover');
+			}
+		});
+		$('.slides_list a').first().click();
+	}
 });
